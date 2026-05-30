8e01edc40811f0d72a8dcbbc87e2e174abd22642
diego munoz <omunoz.diego@hotmail.com>
2026-05-30 18:11:46 +0100

    fix: restringir CORS al origen real de Amplify en produccion

--- backend/list-expenses/app.ts
+++ backend/list-expenses/app.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { getExpensesByUser } from './services/expenseRepository';
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': 'https://main.d1bfigmggittui.amplifyapp.com',
   'Access-Control-Allow-Headers': 'Content-Type',
   'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
 };
